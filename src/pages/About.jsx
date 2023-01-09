96fa70f29102b74a21be37f86e14087b91ec88a5
Cristina SG <cristinasanchezg@gmail.com>
2023-01-08 19:06:38 -0600

    About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,22 @@
+import React from 'react';
+// import Hero from "../components/Hero";
+
 const About = () => {
     return (
-        <h1>Componente about</h1>
+        <main>
+            {/* <Hero /> */}
+            <div className="container mt-50">
+                <div className="col-lg-6 pb-5">
+                    <h4 className="mb-4">Cerveza Artesanal Jaliscience</h4>
+                    <p className="mb-5"> Contamos con 3 estilos base que son American Blonde Ale, Porter Blonde Ale y la premiada Mole Porter. Cada cerveza es realizada con el mayor cuidado desde la selección de materia prima hasta el embotellado para entregar productos de calidad que te acompañaran en los mejores momentos de tu vida. </p>
+                    
+                </div>
+            </div>
+
+            {/* <h1>Aqui va ir el contenido del home</h1> */ }
+        </main >
     );
 }
+
 
 export default About;
