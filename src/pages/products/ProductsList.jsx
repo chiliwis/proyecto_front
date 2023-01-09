7e7e1aad5d95de50fbd24973c960a61aa342798c
Cristina SG <cristinasanchezg@gmail.com>
2023-01-08 22:29:06 -0600

    Quitar variables no usadas

--- src/pages/products/ProductsList.jsx
+++ src/pages/products/ProductsList.jsx
@@ -1,5 +1,5 @@
 import Hero from "../../components/Hero";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import { getProducts } from "../../services/Product.service";
 
